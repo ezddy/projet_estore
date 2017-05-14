7337e50fbfcfe31448fa81b903ec99c7ce10b80e
davidha <david.ha.20160073@efrei.net>
2017-05-14 14:10:26 -0400

    add new

---
+++ form_registration.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Registration</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<form action="registration.php" method="POST">
+		<label for="username">Your username</label>
+		<input type="text" id="username" name="username"><br>
+		<label for="password">Your password</label>
+		<input type="password" id="password" name="password"><br>
+		<button type="submit">Register!</button>
+	</form>
+</body>
+</html>
