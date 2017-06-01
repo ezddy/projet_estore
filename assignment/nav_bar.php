7c81fe60c99ad7fe9fda360a42b3a329b1ec9066
davidha <david.ha.20160073@efrei.net>
2017-05-31 23:48:02 -0400

    clean files

--- assignment/nav_bar.php
+++
@@ -1,7 +0,0 @@
-<div id="menu">
-	<ul>
-		<li><a href="reset_database.php">Reset database</a></li>
-		<li><a href="insert_question.php">Insert questions</a></li>
-		<li><a href="exam_page.php">Exam</a></li>
-	</ul>
-</div>
